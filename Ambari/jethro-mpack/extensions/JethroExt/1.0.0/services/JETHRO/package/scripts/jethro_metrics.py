eb2161daa6de5d8a96d1b56609c44ec389bac23a
izik <izikorgad@gmail.com>
2017-11-29 13:09:46 +0200

    WEBUI-740: widget does not recognize a new instance created from the WEBUI on the same machine. WEBUI-746: wrong alerts on secondary instance

--- Ambari/jethro-mpack/extensions/JethroExt/1.0.0/services/JETHRO/package/scripts/jethro_metrics.py
+++ Ambari/jethro-mpack/extensions/JethroExt/1.0.0/services/JETHRO/package/scripts/jethro_metrics.py
@@ -11,7 +11,7 @@ from jethro_metrics_utils import read_metric_values, instance_name_to_number
 
 class JethroMetrics():
 
-    metrics = []
+    # metrics = []
     metrics_dict = dict()
     ams_collector_address = "0.0.0.0:6188"
     headers = {"Content-type": "application/json"}
@@ -22,11 +22,14 @@ class JethroMetrics():
         self.time_now = int(time.time() * 1000.0)
         self.ams_collector_address = ams_collector_address
 
-    def submit_metrics(self, appid, metric_name, metric_value):
+    def submit_metrics(self, appid, metric_name, metric_values):
+        
+        self.metrics = []
+
         self.time_now = int(time.time() * 1000.0)
         self.my_hostname = socket.getfqdn()
 
-        self.metrics.append(self.get_metrics(appid, metric_name, metric_value))
+        self.metrics.append(self.get_metrics(appid, metric_name, metric_values))
         self.metrics_dict["metrics"] = self.metrics
         json_string = json.JSONEncoder().encode(self.metrics_dict)
 
@@ -46,16 +49,20 @@ class JethroMetrics():
             print("Unable to get a response from Ambari Metric Collector: " + str(e))
         connection.close()
 
-    def get_metrics(self, appid, metric_name, metric_value):
+    def get_metrics(self, appid, metric_name, metric_values):
+        vals = {}
+        i = 0
+        for val in metric_values:
+            vals[str(self.time_now + (i * 1000))] = val
+            i = i +1
+
         metric = {
             "metricname": metric_name,
             "appid": appid,
             "hostname": self.my_hostname,
             "timestamp": self.time_now,
             "starttime": self.time_now,
-            "metrics": {
-                str(self.time_now): metric_value
-            }
+            "metrics": vals
         }
         return metric
 
@@ -64,9 +71,11 @@ class JethroMetrics():
 
 def submit_attached_instances_names_metrics():
     res = os.popen("awk -F \":\" '$1 !~ /#/ {x=$1} {if (x != \"\") print x}' /opt/jethro/instances/services.ini")
+    instances = []
     for instance_name in res:
         instance_num = instance_name_to_number(instance_name.replace('LF', ''))
-        jethro_metrice_collector.submit_metrics('jethro_mng', 'attached_instances_names', instance_num)
+        instances.append(instance_num)
+    jethro_metrice_collector.submit_metrics('jethro_mng', 'attached_instances_names', instances)
 
 
 def format_instance_name(instance_name):
@@ -83,8 +92,7 @@ def submit_running_instances_names_metrics():
         if instance_num not in instances:
             instances.append(instance_num)
 
-    for instance_num in instances:
-        jethro_metrice_collector.submit_metrics('jethro_mng', 'running_instances_names', instance_num)
+    jethro_metrice_collector.submit_metrics('jethro_mng', 'running_instances_names', instances)
 
 
 def submit_running_instances_metrics():
@@ -92,35 +100,47 @@ def submit_running_instances_metrics():
     metrics = read_metric_values(ams_host, ams_port, False, "jethro_mng", "running_instances_names", 3)
 
     val = len(set(metrics))
+    vals = []
+    vals.append(val)
 
-    jethro_metrice_collector.submit_metrics('jethro_mng', 'running_instances', val)
+    jethro_metrice_collector.submit_metrics('jethro_mng', 'running_instances', vals)
 
 
 def submit_maint_status_metrics():
     res = os.popen("service jethro status | awk '/JethroMaint/ {print $2}'")
+    instances = []
     for instance in res:
         instance_name = format_instance_name(instance)
         instance_num = instance_name_to_number(instance_name)
-        jethro_metrice_collector.submit_metrics('jethro_maint', 'running_maint_services', instance_num)
+        instances.append(instance_num)
+    jethro_metrice_collector.submit_metrics('jethro_maint', 'running_maint_services', instances)
 
 
 def submit_load_scheduler_status_metrics():
     res = os.popen("service jethro status | awk '/JethroLoadsScheduler/ {print $2}'")
+    instances = []
     for instance in res:
         instance_name = format_instance_name(instance)
         instance_num = instance_name_to_number(instance_name)
-        jethro_metrice_collector.submit_metrics('jethro_load_scheduler', 'running_load_scheduler_services', instance_num)
+        instances.append(instance_num)
+        jethro_metrice_collector.submit_metrics('jethro_load_scheduler', 'running_load_scheduler_services', instances)
 
 
 def submit_instance_storage_metrics(jethro_user):
     res = os.popen("awk -F \":\" '$1 !~ /#/ {x=$1} {if (x != \"\") print x}' /opt/jethro/instances/services.ini")
     storage_sum = 0.0
     for instance_name in res:
-        storage_path = os.popen("awk -F \"=\" '$1 ~ /storage.root.path/ {print $2}' /opt/jethro/instances/" + instance_name.replace('\n', '') + "/local-conf.ini").read()
-        storage_usage = os.popen(format('su - {jethro_user} -c "hadoop fs -du -s -h {storage_path}"') + " | awk '{if ($2 == \"K\") print $1 / 1000 / 1000; else if ($2 == \"M\") print $1 / 1000; else print $1}'").read()
+        storage_path = os.popen(
+            "awk -F \"=\" '$1 ~ /storage.root.path/ {print $2}' /opt/jethro/instances/" + instance_name.replace('\n', '') + "/local-conf.ini").read()
+        storage_usage = os.popen(format('su - {jethro_user} -c "hadoop fs -du -s -h {storage_path}"') +
+                                 " | awk '{if ($2 == \"K\") print $1 / 1000 / 1000; else if ($2 == \"M\") print $1 / 1000; else print $1}'").read()
         storage_sum += float(storage_usage)
 
-    jethro_metrice_collector.submit_metrics('jethro_maint', 'instance_storage_size_gb', storage_sum)
+    vals = []
+    vals.append(storage_sum)
+
+    jethro_metrice_collector.submit_metrics(
+        'jethro_maint', 'instance_storage_size_gb', vals)
 
 #****************************************************************
 
@@ -152,7 +172,7 @@ while True:
         submit_running_instances_names_metrics()
         submit_running_instances_metrics()
         submit_instance_storage_metrics(jethro_user)
-        
+
     except Exception as e:
         print("Unable to submit Jethro metrics to Ambari Metric Collector: " + str(e))
     finally:
